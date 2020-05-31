3eb0f0b540c03613fb17f7b33b1d6366ac8c3c32
Pramod Vishwakarma <pramodvishwakarma12@hotmail.com>
2020-05-31 17:57:28 +0530

    nova watch project

---
+++ js/slicknew.js
@@ -0,0 +1,31 @@
+$('#slide').slick({
+          dots: false,
+          infinite: false,
+          speed: 500,
+          slidesToShow:5,
+          slidesToScroll: 1,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            ]
+         });
+ 
